0ae0ed9860f02fc0d73e6534a46a7e716c6726bf
sumeet <sumeetsaini621@gmail.com>
2025-11-16 10:56:16 +0000

    day 4 tutorial 1

---
+++ day-4/agent-observability.py
@@ -0,0 +1,102 @@
+##
+import os
+import logging
+
+try:
+    GOOGLE_API_KEY = os.environ['GOOGLE_API_KEY']
+    print("Key SUCCESS!")
+except Exception as e:
+    print("Couldn't find your key")
+
+# Clean up any previous logs
+for log_file in ["logger.log", "web.log", "tunnel.log"]:
+    if os.path.exists(log_file):
+        os.remove(log_file)
+        print(f"🧹 Cleaned up {log_file}")
+
+# Configure logging with DEBUG log level.
+logging.basicConfig(
+    filename="logger.log",
+    level=logging.DEBUG,
+    format="%(filename)s:%(lineno)s %(levelname)s:%(message)s",
+)
+
+## Create research agent (who's code I have edited manually!)
+!adk create research-agent --model gemini-2.5-flash-lite --api_key $GOOGLE_API_KEY
+
+## Run the ADK web UI
+!adk web --log_level DEBUG
+
+## Local Logs
+!cat logger.log > log.txt
+
+## USING LOGGING PLUGIN
+# Define the same agent as in research-agent/agent.py
+from google.adk.agents import LlmAgent
+from google.adk.models.google_llm import Gemini
+from google.adk.tools.agent_tool import AgentTool
+from google.adk.tools.google_search_tool import google_search
+
+from google.genai import types
+from typing import List
+
+retry_config = types.HttpRetryOptions(
+    attempts=5,  # Maximum retry attempts
+    exp_base=7,  # Delay multiplier
+    initial_delay=1,
+    http_status_codes=[429, 500, 503, 504],  # Retry on these HTTP errors
+)
+
+
+def count_papers(papers: List[str]):
+    """
+    This function counts the number of papers in a list of strings.
+    Args:
+      papers: A list of strings, where each string is a research paper.
+    Returns:
+      The number of papers in the list.
+    """
+    return len(papers)
+
+
+# Google search agent
+google_search_agent = LlmAgent(
+    name="google_search_agent",
+    model=Gemini(model="gemini-2.5-flash-lite", retry_options=retry_config),
+    description="Searches for information using Google search",
+    instruction="Use the google_search tool to find information on the given topic. Return the raw search results.",
+    tools=[google_search],
+)
+
+# Root agent
+research_agent_with_plugin = LlmAgent(
+    name="research_paper_finder_agent",
+    model=Gemini(model="gemini-2.5-flash-lite", retry_options=retry_config),
+    instruction="""Your task is to find research papers and count them. 
+   
+   You must follow these steps:
+   1) Find research papers on the user provided topic using the 'google_search_agent'. 
+   2) Then, pass the papers to 'count_papers' tool to count the number of papers returned.
+   3) Return both the list of research papers and the total number of papers.
+   """,
+    tools=[AgentTool(agent=google_search_agent), count_papers],
+)
+
+## Adding the LoggingPlugin
+from google.adk.runners import InMemoryRunner
+from google.adk.plugins.logging_plugin import (
+    LoggingPlugin,
+)  # <---- 1. Import the Plugin
+from google.genai import types
+import asyncio
+
+runner = InMemoryRunner(
+    agent=research_agent_with_plugin,
+    plugins=[
+        LoggingPlugin()
+    ],  # <---- 2. Add the plugin. Handles standard Observability logging across ALL agents
+)
+
+## Run the agent and see the logging!
+response = await runner.run_debug("Find recent papers on quantum computing")
+
